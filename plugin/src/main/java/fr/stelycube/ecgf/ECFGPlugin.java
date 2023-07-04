79d7021439328670b76dd57a4f0e3b0a4a5994b5
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2023-07-05 00:30:45 +0200

    Add ConfigLoader

--- plugin/src/main/java/fr/stelycube/ecgf/ECFGPlugin.java
+++ plugin/src/main/java/fr/stelycube/ecgf/ECFGPlugin.java
@@ -1,12 +1,29 @@
 package fr.stelycube.ecgf;
 
+import fr.stelycube.ecgf.config.ConfigLoader;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @SuppressWarnings("unused")
 public class ECFGPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        final Logger logger = getLogger();
+        final File dataFolder = getDataFolder();
+        final ConfigLoader configLoader = new ConfigLoader();
+
+        if (!configLoader.checkDataFolder(dataFolder)) {
+            logger.log(Level.WARNING, "Can not write in the directory : " + dataFolder.getAbsolutePath());
+            logger.log(Level.WARNING, "Disable the plugin");
+            Bukkit.getPluginManager().disablePlugin(this);
+            return;
+        }
+
 
     }
 
